7fb2e3fb9d3960ceee4527ad4065878533fccfa8
Rugumbira Jordy Bastien <rugumbirajordybastien@gmail.com>
2020-04-05 11:37:40 +0200

    Actions

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitialData } from '../utils/api';
+import { receiveUsers } from './users';
+import { receiveTweets } from './tweets';
+import { setAuthedUser } from './autherUser';
+
+const AUTHED_ID = 'tylermcginnis';
+
+export const handleInitialData = () => {
+  return (dispatch) => {
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
+  };
+};
